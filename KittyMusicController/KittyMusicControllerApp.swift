327a104dd49e253e8ad7bd42368f7d5ec9bf4542
Jenna <jenna@jenna-studio.dev>
2026-04-24 10:40:04 +0900

    Initial Commit

---
+++ KittyMusicController/KittyMusicControllerApp.swift
@@ -0,0 +1,17 @@
+//
+//  KittyMusicControllerApp.swift
+//  KittyMusicController
+//
+//  Created by Jenna on 2026-04-24.
+//
+
+import SwiftUI
+
+@main
+struct KittyMusicControllerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
